5546efdfbb6e61b0d102b48342c9a613e5da34cf
Asbjørn Juhl Holm Pedersen <asbj1702@elev.heguddannelser.dk>
2023-02-17 11:56:04 +0100

    Add project files.

---
+++ rougeLikeADarkRoom/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace rougeLikeADarkRoom
+{
+    internal class ItemClasses
+    {
+        public enum Catogory
+        {
+            Healing,
+            Weaponing,
+        }
+        public enum Healing
+        {
+            bread,
+            healPotion,
+            bandage,
+            cocio
+        }
+        public HealClass  { get; set; }
+
+    public int durability { get; set; }
+
+}
+}
---
+++ rougeLikeADarkRoom/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace rougeLikeADarkRoom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ItemClasses itemClasses = new ItemClasses();
+
+            while (true)
+            {
+                break;
+            }
+        }
+    }
+}
