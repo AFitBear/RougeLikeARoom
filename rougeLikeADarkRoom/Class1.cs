33c455fb61d1d64b722ebfa59fc5af3bcf494dec
Asbjørn Juhl Holm Pedersen <asbj1702@elev.heguddannelser.dk>
2023-02-17 13:42:01 +0100

    gg

--- rougeLikeADarkRoom/Class1.cs
+++ rougeLikeADarkRoom/Class1.cs
@@ -15,9 +15,11 @@
             bandage,
             cocio
         }
-        public HealClass  { get; set; }
-
-    public int durability { get; set; }
+        public Healing heiling { get; set; }
+        public string name { get; set; }
+        
+        
+        public int durability { get; set; }
     
 }
 }
--- rougeLikeADarkRoom/Program.cs
+++ rougeLikeADarkRoom/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("gg");
             ItemClasses itemClasses = new ItemClasses();
 
             while (true)
