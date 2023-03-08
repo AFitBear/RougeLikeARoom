7618b5c534fa3c97a3ddda3b79c109805aaa3eec
Asbjørn Juhl Holm Pedersen <asbj1702@elev.heguddannelser.dk>
2023-03-08 12:28:27 +0100

    new public int moment

--- rougeLikeADarkRoom/Class1.cs
+++ rougeLikeADarkRoom/Class1.cs
@@ -1,11 +1,30 @@
 ﻿namespace rougeLikeADarkRoom
 {
+    internal class Board
+    {
+        public static void PrintBoard(int rowAndCol)
+        {
+            for (int row = 0; row < rowAndCol - 1; row++)
+            {
+                for (int col = 0; col < rowAndCol - 1; col++)
+                {
+                    if (row == 0 || col == 0 || row == rowAndCol - 2 || col == rowAndCol - 2)
+                    {
+                        Console.SetCursorPosition(col, row);
+                        Console.Write('-');
+                    }
+                }
+            }
+        }
+    }
+
     internal class ItemClasses
     {
         public enum Catogory
         {
             Healing,
-            Weaponing
+            Weaponing,
+            miscing
         }
         public Catogory Catogorie { get; set; }
         public enum Healing
@@ -16,10 +35,12 @@
             cocio
         }
         public Healing heiling { get; set; }
+
         public string name { get; set; }
-        
-        
+
+
         public int durability { get; set; }
-    
+
+    }
 }
-}
+
--- rougeLikeADarkRoom/Program.cs
+++ rougeLikeADarkRoom/Program.cs
@@ -2,18 +2,38 @@
 {
     internal class Program
     {
+        static public int size;
         static void Main(string[] args)
         {
             Console.WriteLine("gg");
+            /*int rows = 20;
+            int cols = rows;*/
+            int length = 20;
+            size= length;
+
             ItemClasses itemClasses = new ItemClasses();
+            Board boardPrint = new Board();
+            Board.PrintBoard(size);
+
+
+
+            const float TARGET_FPS = 4f; // Frame rate (ønsket)
+            const float TARGET_FRAME_TIME = 1f / TARGET_FPS; // Tid per frame
+            DateTime previousFrameTime;
+            float elapsedTime = 0f;
 
             while (true)
             {
+                previousFrameTime = DateTime.UtcNow;
+                elapsedTime = (float)(DateTime.UtcNow - previousFrameTime).TotalSeconds;
+                if (elapsedTime < TARGET_FRAME_TIME)
                 {
-                    new ItemClasses { Catogorie = ItemClasses.Catogory.Healing, heiling = ItemClasses.Healing.bread, durability = 6 };
-                    
-                }   
+                    // Hvis en frame tid ikke er forløbet endnu, sov lidt:
+                    int sleepTime = (int)((TARGET_FRAME_TIME - elapsedTime) * 1000);
+                    System.Threading.Thread.Sleep(sleepTime);
+                }
             }
+            //new ItemClasses { Catogorie = ItemClasses.Catogory.Healing, heiling = ItemClasses.Healing.bread, durability = 6 };
         }
     }
 }
