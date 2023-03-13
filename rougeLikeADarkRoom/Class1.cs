d7e0d5ccd750367e1235ba09a1c88ca1f470d0b7
Asbjørn Juhl Holm Pedersen <asbj1702@elev.heguddannelser.dk>
2023-03-13 15:00:57 +0100

    Needs to work on arrowkays iynwim

--- rougeLikeADarkRoom/Class1.cs
+++ rougeLikeADarkRoom/Class1.cs
@@ -4,6 +4,7 @@
     {
         public static void PrintBoard(int rowAndCol)
         {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
             for (int row = 0; row < rowAndCol - 1; row++)
             {
                 for (int col = 0; col < rowAndCol - 1; col++)
@@ -11,9 +12,60 @@
                     if (row == 0 || col == 0 || row == rowAndCol - 2 || col == rowAndCol - 2)
                     {
                         Console.SetCursorPosition(col, row);
-                        Console.Write('-');
+                        Console.Write('\u25ae');
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(col, row);
+                        Console.WriteLine(".");
                     }
                 }
+            }
+        }
+    }
+    internal class person
+    {
+
+        public static void action(ref int x, ref int y)
+        {
+            ConsoleKeyInfo key = Console.ReadKey();
+            switch (key.Key)
+            {
+                case ConsoleKey.UpArrow or ConsoleKey.W:
+                    if (y > 2)
+                    {
+                        y -= 1;
+                        Console.SetCursorPosition(x, y);
+                        Console.Write('@');
+                    }
+                    break;
+                case ConsoleKey.DownArrow or ConsoleKey.S:
+                    if (y < 22)
+                    {
+                        y += +1;
+                        Console.SetCursorPosition(x, y);
+                        Console.Write('@');
+                    }
+                    break;
+                case ConsoleKey.RightArrow or ConsoleKey.D:
+                    x += +1;
+                    if (x < 22)
+                    {
+                        Console.SetCursorPosition(x, y);
+                        Console.Write('@');
+                    }
+                    break;
+                case ConsoleKey.LeftArrow or ConsoleKey.A:
+                    x -= 1;
+                    if (x > 1)
+                    {
+                        Console.SetCursorPosition(x, y);
+                        Console.Write('@');
+                    }
+                    break;
+                default:
+                    break;
+
             }
         }
     }
--- rougeLikeADarkRoom/Program.cs
+++ rougeLikeADarkRoom/Program.cs
@@ -1,29 +1,43 @@
-﻿namespace rougeLikeADarkRoom
+﻿using System;
+
+namespace rougeLikeADarkRoom
 {
     internal class Program
     {
         static public int size;
+        static public int xPorsition;
+        static public int yPorsition;
+            
+
         static void Main(string[] args)
         {
             Console.WriteLine("gg");
             /*int rows = 20;
             int cols = rows;*/
-            int length = 20;
+            int length = 23;
             size= length;
-
+           //makes the board
             ItemClasses itemClasses = new ItemClasses();
             Board boardPrint = new Board();
             Board.PrintBoard(size);
-
-
-
-            const float TARGET_FPS = 4f; // Frame rate (ønsket)
+            /*StreamWriter saveFile = new StreamWriter(@"boardSize.csv");
+           StreamReader loadFile = new StreamReader(@"boardSize.csv");
+           //Saves the file to long term memmery(my ssd methinks) it also closes the file so i cant write to it
+           saveFile.Close();
+           saveFile.WriteLine(size);
+           int tinkk;
+           tinkk = int.Parse(loadFile.ReadLine());
+           */
+            //start position is the middle of the board
+            xPorsition %= 2;
+            yPorsition %= 2;
+            const float TARGET_FPS = 24; // Frame rate (ønsket)
             const float TARGET_FRAME_TIME = 1f / TARGET_FPS; // Tid per frame
             DateTime previousFrameTime;
             float elapsedTime = 0f;
-
             while (true)
             {
+                person.action(ref xPorsition,ref yPorsition);
                 previousFrameTime = DateTime.UtcNow;
                 elapsedTime = (float)(DateTime.UtcNow - previousFrameTime).TotalSeconds;
                 if (elapsedTime < TARGET_FRAME_TIME)
