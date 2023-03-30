3e3c44d3c1cc518eb186b339941577dcc169bbe6
Asbjørn Juhl Holm Pedersen <asbj1702@elev.heguddannelser.dk>
2023-03-30 15:00:58 +0200

    made the method to activate action. will make action later.

--- rougeLikeADarkRoom/Class1.cs
+++ rougeLikeADarkRoom/Class1.cs
@@ -14,21 +14,16 @@
     {
         public enum Types
         {
-            wall,
-            chest,
-            unexpored,
-            explored,
-            bandit,
-            boss
+            wall = 0,
+            chest = 1,
+            unexplored = 2,
+            explored = 3,
+            bandit = 4,
+            boss = 5
 
 
         }
-        public Types theTileType;
-        public TileType(Types type)
-        {
-            theTileType = type;
-        }
-
+        public Types theTileType { get; set; }
 
     }
     /*internal class Tyletype
@@ -57,9 +52,6 @@
 
             }
             Random random = new Random();
-            Console.SetCursorPosition(1, 1);
-            Console.Write('B');
-            manyPosition[1][1] = TileType.Types.boss;
             for (int row = 0; row < rowsAndCols - 1; row++)
             {
                 for (int col = 0; col < rowsAndCols - 1; col++)
@@ -77,18 +69,17 @@
 
                             Console.Write("¤");
                             manyPosition[row][col] = TileType.Types.chest;
+                            //laver en chance for at lave en chest eller en bandit osv.
                             switch (random.Next(0, luckyness))
                             {
-                                case < 10:
-                                    Console.WriteLine("under10");
+                                case < 10 when 4 < 5:
+                                    manyPosition[row][col] = TileType.Types.chest;
                                     break;
-                                case > 10:
-                                    Console.WriteLine("over10");
-                                    break;
-                                case 10:
-                                    Console.WriteLine("is10");
+                                case > 9:
+                                    manyPosition[row][col] = TileType.Types.chest;
                                     break;
                                 default:
+                                    manyPosition[row][col] = TileType.Types.unexplored;
                                     break;
                             }
 
@@ -97,17 +88,79 @@
                         else
                         {
                             Console.WriteLine(".");
-                            manyPosition[row][col] = TileType.Types.unexpored;
+                            manyPosition[row][col] = TileType.Types.unexplored;
                         }
                     }
                 }
             }
+            Console.SetCursorPosition(1, 1);
+            Console.Write(GetTileTypeInfo(TileType.Types.boss).logo);
+            manyPosition[1][1] = TileType.Types.boss;
+
         }
+        public static (string name, char logo, int number) GetTileTypeInfo(TileType.Types theTile)
+        {
+            switch (theTile)
+            {
+                case TileType.Types.wall:
+                    return ("wall", '█', 1);
+
+                case TileType.Types.unexplored:
+                    return ("une", '.', 2);
+
+                case TileType.Types.explored:
+                    return ("exp", '+', 3);
+
+                case TileType.Types.chest:
+                    return ("chest", 'c', 4);
+
+                case TileType.Types.bandit:
+                    return ("band", 'b', 5);
+
+                case TileType.Types.boss:
+                    return ("boss", 'B', 6);
+                default:
+                    return ("null", '?', 0);
+            }
+        }
+        public static void CheckAction(int actionThing)
+        {
+            //checks what action need to be done. like if you are on a bandit tile then i does bandit action
+            switch (actionThing)
+            {
+                case 2 or 3:
+
+                break;
+                case 4:
+
+                break;
+                case 5:
+
+                break;
+                case 6:
+
+                break;
+                default:
+                break;
+            }
+        }
+        public static void BanditAction()
+        {
+
+        }
+            public static void ChestAction()
+        {
+
+        }
+            public static void BossAction()
+        {
+
+        }
+
     }
 
     internal class person
     {
-
         public static void action(ref int x, ref int y)
         {
             ConsoleKeyInfo key = Console.ReadKey(true);//There need to be "true" so it intercepts the thing i write.AKA it does not wirte the letter I press down.
@@ -118,25 +171,25 @@
                 case ConsoleKey.UpArrow or ConsoleKey.W:
                     if (y > 1)
                     {
-                        y -= 1;
+                        y--;
                     }
                     break;
                 case ConsoleKey.DownArrow or ConsoleKey.S:
                     if (y < Program.size - 3)
                     {
-                        y += 1;
+                        y++;
                     }
                     break;
                 case ConsoleKey.RightArrow or ConsoleKey.D:
                     if (x < (Program.size - 3))
                     {
-                        x += 1;
+                        x++;
                     }
                     break;
                 case ConsoleKey.LeftArrow or ConsoleKey.A:
                     if (x > 1)
                     {
-                        x -= 1;
+                        x--;
                     }
                     break;
                 default:
@@ -145,6 +198,7 @@
             }
             Console.SetCursorPosition(x, y);
             Console.Write('@');
+
         }
     }
 
--- rougeLikeADarkRoom/Program.cs
+++ rougeLikeADarkRoom/Program.cs
@@ -1,21 +1,35 @@
-﻿using System;
-
-namespace rougeLikeADarkRoom
+﻿namespace rougeLikeADarkRoom
 {
     internal class Program
     {
         static public int size;
         static public int xPorsition;
         static public int yPorsition;
-            
+        static public int difficulty = 1;
 
         static void Main(string[] args)
         {
+            Console.SetCursorPosition(0, 50);
+            Console.WriteLine("My name is Yoshikage Kira. I'm 33 years old. ");
+            Console.WriteLine("My house is in the northeast section of Morioh, where all the villas are, and I am not married.");
+            Console.WriteLine("I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest.");
+            Console.WriteLine("I don't smoke, but I occasionally drink. ");
+            Console.WriteLine("I’m in bed by 11 PM, and make sure I get eight hours of sleep, no matter what.");
+            Console.WriteLine("After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, ");
+            Console.WriteLine("I usually have no problems sleeping until morning.");
+            Console.WriteLine("Just like a baby, I wake up without any fatigue or stress in the morning. ");
+            Console.WriteLine("I was told there were no issues at my last check-up.");
+            Console.WriteLine("I’m trying to explain that I’m a person who wishes to live a very quiet life.");
+            Console.WriteLine("I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night.");
+            Console.WriteLine("That is how I deal with society, and I know that is what brings me happiness. ");
+            Console.WriteLine("Although, if I were to fight I wouldn’t lose to anyone.");
+
+
             Console.CursorVisible = false;
             //int rows = 20;
             //int cols = rows;
             size = 23;
-           //makes the board
+            //makes the board
             ItemClasses itemClasses = new ItemClasses();
             Board boardPrint = new Board();
             Board.PrintBoard(size);
@@ -28,8 +42,8 @@ namespace rougeLikeADarkRoom
            tinkk = int.Parse(loadFile.ReadLine());
            */
             //start position is the middle of the board
-            xPorsition = size/2;
-            yPorsition = size/2;
+            xPorsition = size / 2;
+            yPorsition = size / 2;
             /*const float TARGET_FPS = 24; // Frame rate (ønsket)
             const float TARGET_FRAME_TIME = 1f / TARGET_FPS; // Tid per frame
             DateTime previousFrameTime;
@@ -37,7 +51,7 @@ namespace rougeLikeADarkRoom
             //game loop
             while (true)
             {
-                person.action(ref xPorsition,ref yPorsition);
+                person.action(ref xPorsition, ref yPorsition);
                 /*previousFrameTime = DateTime.UtcNow;
                 elapsedTime = (float)(DateTime.UtcNow - previousFrameTime).TotalSeconds;
                 if (elapsedTime < TARGET_FRAME_TIME)
